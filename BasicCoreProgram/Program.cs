1ea01c69ab2aa74bcc97e01f7f18d7e3a4ef26bf
“Enter <anirudhanand984@gmail.com>
2021-11-14 13:22:10 +0530

    [Anirudh] add : created Swapp two Numbers Program

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -9,7 +9,7 @@ namespace BasicCoreProgram
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainder \n 7. Exit");
+                Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainder \n 7. SwapTwoNumers \n 8. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -38,6 +38,10 @@ namespace BasicCoreProgram
                         result.Division();
                         break;
                     case 7:
+                        Swapping nos = new Swapping();
+                        nos.Swapp();
+                        break;
+                    case 8:
                         flag = false;
                         break;
                 }
---
+++ BasicCoreProgram/SwappingTwoNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCoreProgram
+{
+    public class Swapping
+    {
+        public void Swapp()
+        {
+            Console.WriteLine("Enter the First number :");
+            int First_Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number :");
+            int Second_Number = Convert.ToInt32(Console.ReadLine());
+            First_Number = First_Number + Second_Number;
+            Second_Number = First_Number - Second_Number;
+            First_Number = First_Number - Second_Number;
+            Console.WriteLine("After Swapping these Two Numbers : First number is =" + First_Number + " and Second number is =" + Second_Number);
+        }
+    }
+}
